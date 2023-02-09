147032932c67b704605f0c49db1f092b6b1f264b
Artur Alves <22282175@CFP127ALUNO.LOCAL>
2023-02-09 16:55:27 -0300

    aula4

--- aula3 - pt2/app.js
+++ aula3 - pt2/app.js
@@ -7,42 +7,42 @@
 
 var matematica = require('./modulo/calculadora.js')
 
- var readLine = require('readline');
+var readLine = require('readline');
 
- var entradaDados = readLine.createInterface({
-     input : process.stdin,
-     output : process.stdout
- });
+var entradaDados = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
- entradaDados.question('Valor: \n', function(numero1){
+entradaDados.question('Valor: \n', function (numero1) {
 
-    let valor1 = numero1.replace(',' , '.');
+    let valor1 = numero1.replace(',', '.');
 
-    entradaDados.question('Valor2: \n', function(numero2){
-        
-        let valor2 = numero2.replace(',' , '.');
+    entradaDados.question('Valor2: \n', function (numero2) {
 
-        entradaDados.question('Escolha uma operação matemática: [SOMAR | SUBTRAIR | MULTIPLICAR | DIVIDIR] \n', function(tipoCalc){
-            
+        let valor2 = numero2.replace(',', '.');
+
+        entradaDados.question('Escolha uma operação matemática: [SOMAR | SUBTRAIR | MULTIPLICAR | DIVIDIR] \n', function (tipoCalc) {
+
             let operacao = tipoCalc.toUpperCase();
             let resultado;
 
-            if(valor1 == '' || valor2 == '' || operacao == ''){
+            if (valor1 == '' || valor2 == '' || operacao == '') {
                 console.log('Erro: Não é possível calcular se algum dado estiver em branco.');
 
-            } else if (isNaN(valor1) || isNaN(valor2)){
+            } else if (isNaN(valor1) || isNaN(valor2)) {
                 console.log('Erro: Não é possível calcular se os dados digitados não forem números')
 
-            } else{
-                
+            } else {
+
                 resultado = matematica.calculadora(valor1, valor2, operacao);
-                if(resultado != false)
-                    console.log(resultado);
-                else 
+                if (resultado === false)
                     entradaDados.close();
-                             
+                else
+                    console.log(resultado);
+
             }
         })
     })
 
- })
+})
--- aula3 - pt2/modulo/calculadora.js
+++ aula3 - pt2/modulo/calculadora.js
@@ -5,43 +5,116 @@
  * Version: 1.0
  **/
 
-// Function to realize math operations
-function calculadora(numero1, numero2, tipoCalc) {
-    
+// Function to realize math operations (first way)
+// function calculadora(numero1, numero2, tipoCalc) {
+
+//     let valor1 = Number(numero1);
+//     let valor2 = Number(numero2);
+//     let operacao = tipoCalc.toUpperCase();
+//     let resultado;
+//     let status = true;
+
+//     /*  if (operacao == 'SOMAR') {
+//          resultado = valor1 + valor2;
+ 
+//      } else if (operacao == 'SUBTRAIR') {
+//          resultado = valor1 - valor2;
+ 
+//      } else if (operacao == 'MULTIPLICAR') {
+//          resultado = valor1 * valor2;
+ 
+//      } else if (operacao == 'DIVIDIR') {
+//          resultado = valor1 / valor2;
+//          if (valor2 == 0) {
+//              console.log('Erro: Não é possível realizar a divisão por 0.')
+//              status = false;
+ 
+//          } else {
+//              resultado = valor1 / valor2
+//          }
+ 
+//      } else {
+//          console.log('Erro: a sua escolha de operação matemática foi inválidada');
+//          return false;
+//      } */
+
+//     switch (operacao) {
+//         case 'SOMAR':
+//             resultado = valor1 + valor2;
+//             break;
+//         case 'SUBTRAIR':
+//             resultado = valor1 - valor2;
+//             break;
+//         case 'MULTIPLICAR':
+//             resultado = valor1 * valor2;
+//             break;
+//         case 'DIVIDIR':
+//             if (valor2 == 0) {
+//                 console.log('Erro: Não é possível realizar a divisão por 0.')
+//                 status = false;
+
+//             } else
+//                 resultado = valor1 / valor2
+
+//             break;
+//         default:
+//             console.log('Erro: a sua escolha de operação matemática foi inválidada');
+//             return false;
+
+//     }
+
+//     if (resultado == undefined || resultado == Infinity && status == false) {
+//         status = false;
+//     } else {
+//         return resultado;
+//     }
+// }
+// Second way to create a function
+const calculadora = function(numero1, numero2, tipoCalc){
     let valor1 = Number(numero1);
     let valor2 = Number(numero2);
     let operacao = tipoCalc.toUpperCase();
     let resultado;
+    let status = true;
 
-    if (operacao == 'SOMAR') {
-        resultado = valor1 + valor2;
+    switch (operacao) {
+        case 'SOMAR':
+            resultado = SOMAR(valor1, valor2)
+            break;
+        case 'SUBTRAIR':
+            resultado = SUBTRAIR(valor1, valor2)
+            break;
+        case 'MULTIPLICAR':
+            resultado = MULTIPLICAR(valor1, valor2)
+            break;
+        case 'DIVIDIR':
+            if (valor2 == 0) {
+                console.log('Erro: Não é possível realizar a divisão por 0.')
+                status = false;
 
-    } else if (operacao == 'SUBTRAIR') {
-        resultado = valor1 - valor2;
+            } else
+                resultado = DIVIDIR(valor1, valor2)
 
-    } else if (operacao == 'MULTIPLICAR') {
-        resultado = valor1 * valor2;
+            break;
+        default:
+            console.log('Erro: a sua escolha de operação matemática foi inválidada');
+            return false;
 
-    } else if (operacao == 'DIVIDIR') {
-        resultado = valor1 / valor2;
-        if (valor2 == 0) {
-            console.log('Erro: Não é possível realizar a divisão por 0.')
-
-        } else {
-            resultado = valor1 / valor2
-        }
-
-    } else {
-        console.log('Erro: a sua escolha de operação matemática foi inválidada');
-        entradaDados.close();
     }
-    if (resultado == undefined || resultado == Infinity) {
-        return false;
+
+    if (resultado == undefined || resultado == Infinity && status == false) {
+        status = false;
     } else {
         return resultado;
     }
+
+    
 }
 
+const SOMAR = (valor1, valor2) => valor1 + valor2;
+const SUBTRAIR = (valor1, valor2) => valor1 - valor2;
+const MULTIPLICAR = (valor1, valor2) => valor1 * valor2;
+const DIVIDIR = (valor1, valor2) => valor1 / valor2;
 
 module.exports = {
     calculadora
